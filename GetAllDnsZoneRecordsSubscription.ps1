dee46b7f67e5df68d6dc1eb4a03fc54fc299a49f
dhenrique1555 <85200389+dhenrique1555@users.noreply.github.com>
2024-04-26 12:34:04 -0300

    Create GetAllDnsZoneRecordsSubscription.ps1

---
+++ GetAllDnsZoneRecordsSubscription.ps1
@@ -0,0 +1,29 @@
+$result = @()
+$zone = read-host "Enter Subscription Name"
+$Subscriptions = Get-AzSubscription -subscriptionname 
+ 
+foreach($sub in $subscriptions){
+$zones = @()
+Get-AzSubscription -SubscriptionName $sub.Name | Set-azcontext
+$Zones += Get-AzDnsZone
+ 
+foreach($zone in $zones.name){
+ 
+ 
+$RecordSets = Get-AzDnsRecordSet -ResourceGroupName "rg-dns" -ZoneName $zone
+ 
+foreach($recordset in $recordsets){
+#Export Results to Variable
+$Result += New-Object PSObject -property $([ordered]@{ 
+SubscriptionName = $sub.name
+RecordType = $recordset.recordtype
+Name = $recordset.name
+RG = $recordset.resourcegroupname
+Zone = $recordset.zonename
+Records = $recordset.records -join ","
+})
+}
+}
+}
+ 
+$result
