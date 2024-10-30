ab2c9c8ada8ff7bcbbb0a97a5601e800c8230d49
dhenrique1555 <85200389+dhenrique1555@users.noreply.github.com>
2024-10-30 15:31:05 -0300

    Create InstallADDSTemplate.ps1

---
+++ InstallADDSTemplate.ps1
@@ -0,0 +1,19 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSDomainController `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$false `
+-Credential (Get-Credential) `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainName "tayswift1989.com" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
